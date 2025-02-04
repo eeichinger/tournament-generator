c1661945cffa7fea4a97c4b5cdaaf3288bb8bae3
Erich Eichinger <eeichinger@gmail.com>
2025-02-04 10:28:03 +0100

    refactoring and cleanup

--- generate_perms.py
+++ generate_perms.py
@@ -17,6 +17,7 @@ def sortperm(perm: tuple) -> tuple:
         return tuple(perm_s + sorted(perm[6:8]))
     return tuple(perm_s + list(perm[6:7]))
 
+
 # sort segments of 2
 segments_alphabetically_sorted_perms = map(sortperm, raw_perms)
 
@@ -30,52 +31,64 @@ print("---------------")
 print("number of all perms:", len(all_perms))
 
 
-def are_allowed_neighbours(perm1: tuple, perm2: tuple) -> bool:
-    for i in range(0, len(perm1) - 1, 2):
-        is_ok = (((perm1[i] != perm2[i]) and (perm1[i] != perm2[i + 1])) and
-                 ((perm1[i + 1] != perm2[i]) and (perm1[i + 1] != perm2[i + 1])))
-        if not is_ok:
-            return False
-    return True
+class Round:
+    round: tuple[int, ...]
+    handball: tuple[int, int]
+    turmball: tuple[int, int]
+    parcours: tuple[int, int]
+    pause: tuple[int, int]
 
+    def __init__(self, round: tuple[int, ...]):
+        self.round = round
+        self.handball = round[0:2]
+        self.turmball = round[2:4]
+        self.parcours = round[4:6]
+        self.pause = round[6:8]
 
-def is_allowed_next_round(perm1: tuple) -> Callable[[tuple], bool]:
-    def _f(perm2: tuple):
-        return are_allowed_neighbours(perm1, perm2)
+    def __str__(self) -> str:
+        return f"Round[ {self.round} ]"
 
-    return _f
+    def are_allowed_neighbours(self, other: Self) -> bool:
+        perm1 = self.round
+        perm2 = other.round
+        for i in range(0, len(perm1) - 1, 2):
+            is_ok = (((perm1[i] != perm2[i]) and (perm1[i] != perm2[i + 1])) and
+                     ((perm1[i + 1] != perm2[i]) and (perm1[i + 1] != perm2[i + 1])))
+            if not is_ok:
+                return False
+
+        return True
 
 
 class Tournament:
     maxcount: int
-    rounds: list[tuple[int]] = []
+    rounds: list[Round] = []
     plays: dict[int, int] = {}
-    pairs: set[tuple[int, int]] = set()
+    seen_pairings: set[tuple[int, int]] = set()
 
     def __init__(self, maxcount: int):
         self.maxcount = maxcount
 
     def __str__(self) -> str:
-        return f"Tour[ len={self.count()}, rounds={self.rounds}, plays={self.plays} ]"
+        return f"Tour[ len={self.count()}, rounds={[ r.round for r in self.rounds]}, plays={self.plays} ]"
 
-    def append_next_round(self, next_round: tuple[int, ...]) -> Self:
+    def append_next_round(self, next_round: Round) -> Self:
         new_tour = Tournament(self.maxcount)
         new_tour.rounds = self.rounds + [next_round]
-        new_tour.plays =  copy.deepcopy(self.plays)
-        new_tour.pairs =  copy.deepcopy(self.pairs)
+        new_tour.plays = copy.deepcopy(self.plays)
+        new_tour.seen_pairings = copy.deepcopy(self.seen_pairings)
 
-        for i in range(0, len(next_round) - 1, 2):
-            team1: int = next_round[i]
-            team2: int = next_round[i+1]
+        # record seen pairings
+        new_tour.seen_pairings.add(next_round.handball)
+        new_tour.seen_pairings.add(next_round.parcours)
+        new_tour.seen_pairings.add(next_round.turmball)
 
-            if i<6: # only prevent duplicate pairs for competing, not breaks
-                new_tour.pairs.add((team1, team2))
-
+        for i, pair in enumerate([next_round.handball, next_round.parcours, next_round.turmball]):
+            team1: int = pair[0]
+            team2: int = pair[1]
             plays = new_tour.plays.get(i, {})
-            if team1 in next_round[i:i+2]:
-                plays[team1] = plays.get(team1, 0) + 1
-            if team2 in next_round[i:i+2]:
-                plays[team2] = plays.get(team2, 0) + 1
+            plays[team1] = plays.get(team1, 0) + 1
+            plays[team2] = plays.get(team2, 0) + 1
             new_tour.plays[i] = plays
 
         # print(new_tour)
@@ -90,51 +103,46 @@ class Tournament:
     def is_empty(self) -> bool:
         return self.count() == 0
 
-    def latest_round(self) -> tuple[int]:
+    def latest_round(self) -> Round:
         return self.rounds[len(self.rounds) - 1]
 
-    def is_valid_next_round(self, next_round: tuple) -> bool:
+    def is_valid_next_round(self, next_round: Round) -> bool:
         if next_round in self.rounds: return False
 
-        if len(self.rounds)==0:
+        if len(self.rounds) == 0:
             return True
 
-        latest_round=self.rounds[len(self.rounds)-1]
-        if not are_allowed_neighbours(latest_round, next_round):
+        if next_round.handball in self.seen_pairings: return False
+        if next_round.turmball in self.seen_pairings: return False
+        if next_round.parcours in self.seen_pairings: return False
+
+        latest_round = self.rounds[len(self.rounds) - 1]
+        if not latest_round.are_allowed_neighbours(next_round):
             return False
+
         # search for duplicate pairings (same teams play against each other twice)
         # check that noone is playing the same discipline twice - INCL BREAKS!
-        for i in range(0, len(next_round), 2):
-            pairing = next_round[i:i + 2]
-            if pairing in self.pairs: return False
-
-            team1 = next_round[i]
-            team2 = next_round[i+1]
-            for round in self.rounds:
-                if team1 in round[i:i+2]:
-                    if self.plays.get(i, {}).get(team1, 0) > 1: return False
-                if team2 in round[i:i+2]:
-                    if self.plays.get(i, {}).get(team2, 0) > 1: return False
+        for i, pair in enumerate([next_round.handball, next_round.parcours, next_round.turmball]):
+            team1: int = pair[0]
+            team2: int = pair[1]
+            plays = self.plays.get(i, {})
+            if plays.get(team1, 0) > 1: return False
+            if plays.get(team2, 0) > 1: return False
 
         return True
 
 
-def calc_tour(all_perms: list[tuple], current_tour: Tournament) -> list[Tournament]:
+def calc_tour(all_rounds: list[Round], current_tour: Tournament) -> list[Tournament]:
     # print("ct:", current_tour)
     if current_tour.is_complete():
         print("tour complete", current_tour)
         return [current_tour]
-    # if current_tour.is_empty():
-    #     next_rounds = all_perms
-    # else:
-    #     latest_round = current_tour.latest_round()
-    #     next_rounds = filter(is_allowed_next_round(latest_round), all_perms)
 
-    next_rounds = filter(current_tour.is_valid_next_round, all_perms)
+    next_rounds = filter(current_tour.is_valid_next_round, all_rounds)
 
     tours = []
     for next_round in next_rounds:
-        new_tour = calc_tour(all_perms, current_tour.append_next_round(next_round))
+        new_tour = calc_tour(all_rounds, current_tour.append_next_round(next_round))
         tours = tours + new_tour
     return tours
 
@@ -144,10 +152,9 @@ def calc_tour(all_perms: list[tuple], current_tour: Tournament) -> list[Tourname
 
 MAX_TOUR_SIZE = 8
 
-seed_round = all_perms[0]
-
-tournament = Tournament(MAX_TOUR_SIZE) #.append_next_round(tuple(seed_round))
-all_tours = calc_tour(all_perms, tournament)
+all_rounds: list[Round] = [Round(perm) for perm in all_perms]
+tournament = Tournament(MAX_TOUR_SIZE)  # .append_next_round(tuple(seed_round))
+all_tours = calc_tour(all_rounds, tournament)
 
 # print(all_tours)
 print(len(all_tours))
